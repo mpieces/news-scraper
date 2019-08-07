27f4c089afef949786c5e525623a3cc94c57ea4d
Marisa Ramon <mer0522@gmail.com>
2019-08-06 19:17:48 -0500

    change display in home file and add comments to other files

--- config/routes.js
+++ config/routes.js
@@ -5,19 +5,19 @@ var cheerio = require("cheerio");
 var Headline = require("../models/Headline.js");
 var Note = require("../models/Note.js");
 var axios = require('axios');
+
 router.get("/", function(req, res) {
     res.redirect("/all");
   });
   
   // Retrieve data from the db
   router.get("/all", function(req, res) {
-    // Find all results from the scrapedData collection in the db
     Headline.find({}, function(error, found) {
       // Throw any errors to the console
       if (error) {
         console.log(error);
       }
-      // If there are no errors, send the data to the browser as json
+      // If there are no errors, render the home handlebars page with the *art variable accessible to it*
       else {
         var art = {article: found}
         res.render('home', art)
@@ -28,42 +28,44 @@ router.get("/", function(req, res) {
   // Scrape data from one site and place it into the mongodb db
   router.get("/scrape", function(req, res) {
       console.log("inside scrape route")
-    // Make a request via axios for the news section of `ycombinator`
     request("https://www.buzzfeednews.com/",(req, res, body) => {
-      // Load the html body from axios into cheerio
+      // Load the html body into cheerio
       var $ = cheerio.load(body);
-      // For each element with a "title" class
+     
       var titleArray = [];
       $("div.news-feed article").each(function(i, element) {
         var result = {}
         // Save the text and href of each link enclosed in the current element
          result.title = $(element).children("a").children(".newsblock-story-card__info").children("h2.newsblock-story-card__title").text();
-        //console.log("title: " +title)
          result.link = $(element).children("a").attr("href");
          result.photo = $(element).find("a").find("img").attr("src");
       
-        // If this found element had both a title and a link and photo
+        // if the result's title and link are not empty
         if (result.title !== "" && result.link !== "") {
+          // 
           if(titleArray.indexOf(result.title) == -1){
+            // push title into titleArray
             titleArray.push(result.title);
+            // Using Headline model, count results
             Headline.count({title: result.title}, function(err, count){
-              if(count == 0){
+              // if the count is 0 or isn't present
+              if (count == 0) {
+                // create new entry variable = new headline result??
                 var entry = new Headline(result);
-                entry.save(function(err, doc){
-                  if(err){
+                // save entry
+                entry.save(function(err, doc) {
+                  if (err) {
                     console.log(err);
-                  }
-                  else{
+                  } else {
                     console.log(doc);
                   }
                 });
               }
             })
-          }
-          else {
+          } else {
             console.log("Headline already exists");
           }
-        }else {
+        } else {
           console.log("Not saved to DB")
         }
       });
@@ -79,10 +81,11 @@ router.get("/", function(req, res) {
      body: []
    }
 
+// ????
    Headline.findOne({_id: articleId})
    .populate('comment')
-   .exec(function(err, doc){
-     if(err){
+   .exec(function(err, doc) {
+     if (err) {
        console.log(err);
      } else { 
       hbsobj.article = doc;
@@ -99,5 +102,4 @@ router.get("/", function(req, res) {
    
  })
     
-  
-  module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 const router = require('./config/routes');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(router);
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+
 app.listen(PORT, function () {
    console.log("App running and server listening on port:" + PORT);
 });
